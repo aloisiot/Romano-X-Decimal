1739a7d7074aae5bab82f91e80a2be3ddaddd047
aloisiot <loistc.carvalho@gmail.com>
2021-08-20 21:46:38 -0300

    eliminando espaços inuteis

--- src/Converter.java
+++ src/Converter.java
@@ -5,21 +5,21 @@ public class Converter {
 
     private static Romans rElements = new Romans();
     private static List<String[]> romansLists = rElements.getRomansLists();
-    
+
     Converter(){}  
-    
+
     public static String toRoman(int decimal) {
-        
+
         int d = decimal;
         String dString = Integer.toString(d);
         List<String> roman = new ArrayList<String>();
         int positionInRomansLists=0;
         String result = new String();
-        
+
         if(d <= 0 || d >= 3999){
             return null;
         }
-            
+
         for(int j = dString.length() -1 ; j >= 0; j--){
 
             int index;
@@ -27,13 +27,13 @@ public class Converter {
 
             roman.add(romansLists.get(positionInRomansLists)[index]);
             positionInRomansLists++;
-    
+
         }
 
         for(int i = roman.size() - 1; i >= 0 ;i--){
             result+= roman.get(i);
         }
-        
+
         return result;
     }
 

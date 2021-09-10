a681d703301c3c6015addf3aee1ab5c2550db671
aloisiot <loistc.carvalho@gmail.com>
2021-09-09 23:28:45 -0300

    Ajustes no método main visando prover uma boa  experiencia para usuário
    - Add instruções para orientar o usuário durante a interação com o programa;
    
    - Add três diferentes opções de uso do programa para que o usuário possa escolher.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Inteiro para Algarismos Romanos
+# Decimal para Algarismos Romanos
 
 ## Programa para converção de número inteiro em algarismos romanos
+
+Linguagem: Java
--- src/App.java
+++ src/App.java
@@ -1,16 +1,64 @@
+import java.util.Scanner;
+
 public final class App {
 
-    public static void main(String[] args) throws Exception {
+    private static void romansListGenerator(){
 
-        
         for(int i = 1; i < 4000; i++){
 
             String roman = Converter.integerToRoman(i);
 
             System.out.printf("%n%d = %s", i, roman);
         }
-
-        System.out.println("\n\n");
+        
     }
 
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(">>> CONVERTA UM INTEIRO PARA ROMANO OU VICE-VERSA <<<");
+        System.out.println("\nEscolha uma das seguintes opções:");
+        
+        Scanner input = new Scanner(System.in);
+        int opt = 0;
+        int integer;
+        String roman;
+        boolean isValid = false;
+        
+        while(!isValid){
+            System.out.println("Digite");
+            System.out.println("1 para converter um inteiro em romano;");
+            System.out.println("2 para converter um romano em inteiro;");
+            System.out.println("3 para que imprima uma lista com os números de 1 a 3999 em romano. \n");
+            
+            opt = input.nextInt();
+
+            if( 0 < opt || opt > 3)
+                isValid = true;
+            else
+                System.out.println("OPÇÃO INVÁLIDA!");
+
+        }
+        
+        switch(opt){
+            
+            case 1:
+                System.out.println("Insira um número inteiro entre 1 e 3999:");
+                integer = input.nextInt();
+                System.out.printf("Inteiro: %d \nRomano: %s", integer, Converter.integerToRoman(integer));
+                break;
+
+            case 2:
+                System.out.println("Insira um número romano entre 1 e 3999 (todos os caracters devem estar em maiúsculo):");
+                roman = input.next();
+                System.out.printf("Romano: %s \nInteiro: %d", roman, Converter.romanToInteger(roman));
+                break;
+
+            case 3:
+                romansListGenerator();
+        }
+        
+        
+        input.close();
+        System.out.println("\n\n");
+    }
 }
